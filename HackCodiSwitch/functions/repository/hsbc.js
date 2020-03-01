b1ecdd3ad1684eac304c74445f61e6c942a5feb5
Jesus David Mondragon Cirio <david@adhocserv.com>
2020-03-01 08:35:06 -0600

    Se agregaron los métodos para acceder a la API de HSBC

--- HackCodiSwitch/functions/repository/hsbc.js
+++ HackCodiSwitch/functions/repository/hsbc.js
@@ -11,15 +11,15 @@ module.exports = {
         if(!lastname) throw('No lastname provided')
         if(!phoneNumber) throw('No phone number provided')
 
-        let url = `${config.hsbc_url}/account/businesspartners/level2account`
+        let url = `${config.hsbc_url}/hsbc.02.hk.hsbc/accounts`
         let params = {
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'DirectLogin token="eyJhbGciOiJIUzI1NiJ9.eyIiOiIifQ.K8IE_ZoqEvPtwda2ZoGNL-8N9Gn1VQicpJ4CbXOTN1c"'
+                'Authorization': `DirectLogin token="${config.hsbc_token}"`
             },
             body: JSON.stringify({
-                "user_id": "e843d7f9-73b5-4c44-b30f-e57058e6daaf",
+                "user_id": `${config.hsbc_user}`,
                 "label": phoneNumber,
                 "product_code": "AC",
                 "balance": {
@@ -42,94 +42,44 @@ module.exports = {
                 ]
             })
         }
-
-        let body = getBody(name, lastname, phoneNumber, availableFolio)
         
-        /*const answer = await fetch(emtech_url, params)
+        const answer = await fetch(url, params)
         const data = await answer.json()
         if (data.error) {
             throw ({
                 code: data.error.code,
                 message: data.error.message
             })
-        }*/
-
-        let data = {
-            "account_id": "c0115389-03fa-4c46-8760-bfcc9ccf5b98",
-            "user_id": "e843d7f9-73b5-4c44-b30f-e57058e6daaf",
-            "label": "My Account 2",
-            "product_code": "AC",
-            "balance": {
-                "currency": "EUR",
-                "amount": "0.00"
-            },
-            "branch_id": "DERBY6",
-            "account_routings": [
-                {
-                    "scheme": "AccountNumber",
-                    "address": "4930396"
-                }
-            ],
-            "account_attributes": []
         }
-
 
         return data
     },
 
-    async read(
-    ) {
-        let url = `${config.gentera_url}/account/businesspartners/level2account`
-    
-        if(!email) throw('No email provided')
-        if(!password) throw('No password provided')
-    
-        let params = getParams(email, password)
-        const answer = await fetch(url, params)
-    
-        const data = await answer.json()
-    
-        if (data.error) throw (data.error)
-    
-        const decodedToken = await admin.auth().verifyIdToken(data.idToken)
-        const doc = await db.doc(`users/${decodedToken.uid}`).get()
-        
-        let user = {
-            token: data.idToken,
-            id: decodedToken.uid,
-            check_digit: 0,
-            default_check_digit: 0,
-            profile: doc.data().profile? doc.data().profile: {},
-            scheme: doc.data().scheme? doc.data().scheme: {}
+    async read() {
+        let url = `${config.hsbc_url}/users/current`
+        let params = {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `DirectLogin token="${config.hsbc_token}"`
+            },
         }
-        return user
-    },
-
-    get: (userId) => db.doc(`users/${userId}`).get().then((doc) => doc.profile),
-
-    update: (userId, data) => db.doc(`users/${userId}`).update({profile: data}),
-
-    async generateAccess() {
-        let url = `https://dev.apilab.gentera.com.mx/v3/oauth/accesstoken`
-        let params = getParams({
-            "GrantType": "client_credentials",
-	        "ClientSecret": "2Vw1H2fi55BqeZnX"
-        })
         const answer = await fetch(url, params)
+    
         const data = await answer.json()
     
         if (data.error) throw (data.error)
+    
 
-        if(!data.accessToken) throw ('Error token')
+        return data
     },
-
+    
     async getTransactions() {
         let params = {
             method: 'GET', 
             headers: {
                 'Content-Type': 'application/json',
-                'X-API-KEY': 'OA6BLGKP3psAdqiMEA8s6bt8x4vlyRZZ',
-                'Authorization': 'Bearer kxbQSRF9Z2B7us8c10A02GtqGtGc'
+                'Authorization': `DirectLogin token="${config.hsbc_token}"`
             }
         }
         let url = `${config.gentera_url}/${accountId}/transactions`
